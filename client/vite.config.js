3028401fe3cd944f03d451b23197fa52fa8aa5c8
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2026-01-20 10:26:48 -0300

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Headless-Articles-Platform-----Node.js-Express-Sequelize-React-Tailwind-JWT-Authentication/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

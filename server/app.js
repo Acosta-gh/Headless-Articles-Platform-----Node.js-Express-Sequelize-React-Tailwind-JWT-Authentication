be6044418c4dc1e932d7cd3d63f2844c73d23a71
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2026-01-20 03:49:40 -0300

    arreglando cors

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options("*", cors());
+app.options("/", cors());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 
